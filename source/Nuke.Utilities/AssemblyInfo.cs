b01320cb4e8ecae4bdd08603412fab703f123892
Matthias Koch <ithrowexceptions@gmail.com>
2022-08-20 04:55:02 +0200

    Extract Nuke.Utilities.IO.Globbing

--- source/Nuke.Utilities/AssemblyInfo.cs
+++ source/Nuke.Utilities/AssemblyInfo.cs
@@ -6,4 +6,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Nuke.Common")]
 [assembly: InternalsVisibleTo("Nuke.Common.Tests")]
+[assembly: InternalsVisibleTo("Nuke.Utilities.IO.Globbing")]
 [assembly: InternalsVisibleTo("Nuke.Utilities.Tests")]
