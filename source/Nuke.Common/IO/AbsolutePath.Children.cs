22bc1617e2add627406315bae7a3d06b5decc009
Matthias Koch <ithrowexceptions@gmail.com>
2022-08-30 00:43:10 +0200

    Add AbsolutePath extension methods

---
+++ source/Nuke.Common/IO/AbsolutePath.Children.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2022 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Nuke.Common.IO
+{
+    partial class AbsolutePathExtensions
+    {
+        public static IEnumerable<AbsolutePath> GetFiles(
+            this AbsolutePath path,
+            string pattern = "*",
+            SearchOption options = SearchOption.TopDirectoryOnly)
+        {
+            return Directory.GetFiles(path, pattern, options).Select(AbsolutePath.Create);
+        }
+
+        public static IEnumerable<AbsolutePath> GetDirectories(
+            this AbsolutePath path,
+            string pattern = "*",
+            SearchOption option = SearchOption.TopDirectoryOnly)
+        {
+            return Directory.GetDirectories(path, pattern, option).Select(AbsolutePath.Create);
+        }
+    }
+}
---
+++ source/Nuke.Common/IO/AbsolutePath.Exists.cs
@@ -0,0 +1,53 @@
+﻿// Copyright 2022 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Nuke.Common.Utilities;
+
+namespace Nuke.Common.IO
+{
+    public static partial class AbsolutePathExtensions
+    {
+        [Pure]
+        public static bool Exists(this AbsolutePath path, [CallerArgumentExpression("path")] string expression = null)
+        {
+            if (expression.EndsWithOrdinalIgnoreCase("file"))
+                return path.FileExists();
+            if (expression.EndsWithAnyOrdinalIgnoreCase("directory", "dir", "folder"))
+                return path.DirectoryExists();
+
+            throw new ArgumentException($"Cannot infer from argument '{expression}' if either file or directory must exist");
+        }
+
+        [Pure]
+        public static bool FileExists(this AbsolutePath path)
+        {
+            return File.Exists(path);
+        }
+
+        [Pure]
+        public static bool DirectoryExists(this AbsolutePath path)
+        {
+            return Directory.Exists(path);
+        }
+
+        [Pure]
+        public static bool ContainsFile(this AbsolutePath path, string pattern, SearchOption options = SearchOption.TopDirectoryOnly)
+        {
+            Assert.DirectoryExists(path);
+            return path.ToDirectoryInfo().GetFiles(pattern, options).Any();
+        }
+
+        [Pure]
+        public static bool ContainsDirectory(this AbsolutePath path, string pattern, SearchOption options = SearchOption.TopDirectoryOnly)
+        {
+            Assert.DirectoryExists(path);
+            return path.ToDirectoryInfo().GetDirectories(pattern, SearchOption.TopDirectoryOnly).Any();
+        }
+    }
+}
